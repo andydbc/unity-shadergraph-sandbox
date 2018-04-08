a63a8f80b0f4b051bc0b41dd0ebbfe78882b9af2
Andy Duboc <andy.duboc@gmail.com>
2018-04-07 23:53:32 -0400

    Update Pixelate and Hologram Samples

---
+++ Assets/Examples/Pixelate/PixelateController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelateController : MonoBehaviour {
+
+    [SerializeField]
+    Material _material;
+
+	void Update ()
+    {
+        float f = Time.time - Mathf.Floor(Time.time);
+        float t = Mathf.Lerp(0, 128, f);
+
+        _material.SetFloat("Ratio", t);
+        _material.SetFloat("_Ratio", t);
+        _material.SetVector("_Ratio", new Vector4(t, 0,0,0));
+        _material.SetVector("Ratio", new Vector4(t, 0, 0, 0));
+    }
+}
